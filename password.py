f3fd08f25381b37547f30b51c0f8a3889154c479
AlbertZaqaryan <itm.albert.zaqaryan@gmail.com>
2022-05-11 19:47:55 +0400

    password_V1

---
+++ password.py
@@ -0,0 +1,12 @@
+'''My firs password generator :)'''
+
+# import random
+# s = ''
+# while True:
+#     res = random.randint(1, 10)
+#     s += str(res)
+#     if len(s) >= 8:
+#         print(s)
+#         break
+#     else:
+#         continue
